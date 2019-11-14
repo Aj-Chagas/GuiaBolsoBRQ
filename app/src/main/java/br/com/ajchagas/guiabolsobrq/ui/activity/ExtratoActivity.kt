e0d51828d1ddd76602e85605623ed9b950b2cf46
Dylan Pacheco <dylanpachecocas@gmail.com>
2019-11-14 17:01:13 -0300

    alteracoes na tela lista contas e criacao tela extrato

---
+++ app/src/main/java/br/com/ajchagas/guiabolsobrq/ui/activity/ExtratoActivity.kt
@@ -0,0 +1,13 @@
+package br.com.ajchagas.guiabolsobrq.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ajchagas.guiabolsobrq.R
+
+class ExtratoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extrato)
+    }
+}
